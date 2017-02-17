3448dbc5ba7bd062ccf97b5bdc8a48ee35f95be4
Aydar Fayzullin <aydar_fayzullin@yahoo.com>
2017-02-17 16:21:06 -0500

    Add styles to Weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,7 +4,7 @@ var WeatherMessage = ({temp, location}) => {
 // In the case below we're using props instead of this.props because of the arrow function 
     return (
           <div>
-            <h3>It is {temp} in {location}. </h3>
+            <h3 className="text-center">It is {temp} in {location}. </h3>
           </div>  
 		);
 };
